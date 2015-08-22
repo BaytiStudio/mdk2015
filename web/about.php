cab5e3bfa083e52be338bc8e3490d6f56c3639b0
ashram vedanta gandhi <ashram.vedanta.gandhi@gmail.com>
2015-08-23 01:56:15 +0700

    about

---
+++ web/about.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <div class="row text-center">
+      <h1>QOMO
+      <small class="subtitle">Get to know about QOMO</small></h1>
+      <hr>
+      <p><b>Qomo</b> merupakan aplikasi sumber informasi mengenai harga komoditas bahan pangan pokok</p>
+      <p><b>Qomo</b> dapat pula menampilakn harga harga komoditas di berbagai daerah secara realtime</p>
+      <p><b>Qomo</b> dapat membantu pemerintah dengan memberi fitur lapor untuk masyarakat dengan mengakses web atau aplikasi, dengan fitur lapor qomo dapat membantu pemerintah karena data yang ada, didapat dari pelapor yang ada di lapangan</p>
+      <p>
+        <b>Qomo</b> juga memberi feedback untuk pelapor dengan memberi point dan experient yang nantinya dapat ditukarkan
+      </p>
+      <hr>
+      <img src="logobayti.png" alt="logobayti" height="150px" width="150px" />
+      <h1><small class="subtitle">Created By</small>BAYTI</h1>
+  </div>
+</div>
