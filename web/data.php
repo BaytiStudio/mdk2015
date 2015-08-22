f86cc4e7b1da97ca0a2f9c9c00bf12540213b2b3
ashram vedanta gandhi <ashram.vedanta.gandhi@gmail.com>
2015-08-22 20:25:46 +0700

    tambah file ganti index

---
+++ web/data.php
@@ -0,0 +1,46 @@
+<div class="container">
+
+
+    <div class="row">
+
+
+    <div class="col-md-7">
+      <h1>Komoditas</h1><br>
+      <h6>Nasional</h6>
+    </div>
+    <div class="col-md-3">
+      <table>
+        <tr>
+          <h3>harga kemarin</h3>
+        </tr>
+
+        <tr>
+      <h3>harga sekarang</h3>
+    </tr>
+
+      </table>
+
+    </div>
+    <div class="col-md-2">
+      <i class="fa fa-arrow-up" style="margin-top:25%;color:red;"></i>
+      <h5>naik</h5>
+      <i class="fa fa-arrow-down" style="margin-top:25%;color:green;"></i>
+      <h5>turun</h5>
+    </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-8">
+        <h3>kolom berita</h3>
+
+      </div>
+      <div class="col-md-4">
+        <h3>kolom tweet</h3>
+      </div>
+    </div>
+
+<!--
+<br>
+<button type="button" class="btn btn-info">This is a button</button>
+-->
+</div>
