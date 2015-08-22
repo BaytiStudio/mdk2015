d87823313960c9d6a3ef9219a0f40d6eca1cf608
ashram vedanta gandhi <ashram.vedanta.gandhi@gmail.com>
2015-08-23 01:57:55 +0700

    benerin tai kebo

--- web/about.php
+++ web/about.php
@@ -4,7 +4,7 @@
       <small class="subtitle">Get to know about QOMO</small></h1>
       <hr>
       <p><b>Qomo</b> merupakan aplikasi sumber informasi mengenai harga komoditas bahan pangan pokok</p>
-      <p><b>Qomo</b> dapat pula menampilakn harga harga komoditas di berbagai daerah secara realtime</p>
+      <p><b>Qomo</b> dapat pula menampilkan harga komoditas di berbagai daerah secara realtime</p>
       <p><b>Qomo</b> dapat membantu pemerintah dengan memberi fitur lapor untuk masyarakat dengan mengakses web atau aplikasi, dengan fitur lapor qomo dapat membantu pemerintah karena data yang ada, didapat dari pelapor yang ada di lapangan</p>
       <p>
         <b>Qomo</b> juga memberi feedback untuk pelapor dengan memberi point dan experient yang nantinya dapat ditukarkan
