91b576bcfc924834afa37d191ae0b5fc4ee6f832
adhy57 <adhyaksa.herdhianto@gmail.com>
2015-08-23 08:09:23 +0700

    add lapor

--- web/wp/lapor.php
+++ web/wp/lapor.php
@@ -1,3 +1,20 @@
 <?php 
+	include_once '../db/koneksi.php';
+	$nama = $_POST['sNama'];
+	$no_identitas = $_POST['sKtp'];
+	$email = $_POST['sUsername'];
+	$password = $_POST['sPassword'];
+	$jenis = $_POST['sJenis'];
+
+	$sql = "INSERT INTO user (no_identitas, email, nama, password, level, created_at)
+		 VALUES ($no_identitas,'$email','$nama','$password','$jenis',now())";
+	$result = mysql_query($sql);
+	if ($result) {
+		echo "1|Berhasil Daftar";
+	} else {
+		echo "0|Gagal Daftar";
+	}
 	
+
+
  ?>
